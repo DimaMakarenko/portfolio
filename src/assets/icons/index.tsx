42afa757076a91b9a1eabe62c592e3995480764d
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-03-15 16:27:21 +0200

    feat: update iocns and added export file

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,17 @@
+import { ReactComponent as External } from './external.svg';
+import { ReactComponent as Folder } from './folder.svg';
+import { ReactComponent as Github } from './github.svg';
+import { ReactComponent as Gmail } from './gmail.svg';
+import { ReactComponent as LinkedIn } from './linkedin.svg';
+import { ReactComponent as Telegram } from './telegram.svg';
+
+export const icons = {
+  folder: <Folder />,
+  github: <Github />,
+  gmail: <Gmail />,
+  linkedin: <LinkedIn />,
+  telegram: <Telegram />,
+  external: <External />,
+};
+
+export type Icons = keyof typeof icons;
