e1764ff80f8c920890989ac2476c31340fcd13d3
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-11-28 17:03:47 +0200

    fix: update app navigation

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,21 +1,21 @@
 import { ReactElement } from 'react';
-import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
+import { Routes as Switch, Route, HashRouter } from 'react-router-dom';
 
 import { MainPage, ProjectsPage } from 'pages';
 
 export const routes = {
-  main: `/portfolio/`,
-  projects: '/portfolio/projects',
+  main: `/`,
+  projects: '/projects',
 };
 
 export const Routes = (): ReactElement => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path={routes.main} element={<MainPage />} />
         <Route path={routes.projects} element={<ProjectsPage />} />
         <Route path="*" element="" />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
